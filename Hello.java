291458e7d7eee583ad73bd64de9125de6e76d3e4
nikita <nikita@gmail.com>
2026-03-11 21:33:32 +0530

    New hello.java file

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello from GitHub");
         System.out.println("Build triggered using GitHub Webhook");
-        System.out.println("hello i am learning devops...............");
+        System.out.println("hello i am learning devops..");
+        System.out.println("I am Mca Student.......");
     }
 }
---
+++ job1.js
@@ -0,0 +1,2 @@
+console.log("Node.js application running");
+console.log("Build triggered from Jenkins Pipeline");
