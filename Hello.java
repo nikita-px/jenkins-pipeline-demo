04f173fc17e50ea7dfe4734fc54a6349e11f7c78
nikita <nikita@gmail.com>
2026-03-11 21:00:39 +0530

    hello program update.....

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello from GitHub");
         System.out.println("Build triggered using GitHub Webhook");
-        System.out.println("hello i am learning devops...............");
+        System.out.println("hello i am learning devops................");
     }
 }
